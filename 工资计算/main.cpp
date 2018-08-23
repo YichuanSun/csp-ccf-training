4751c2a43ad0bbe522fd6f5070236e9902a081f6
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-23 19:45:26 +0800

    不好做

---
+++ 工资计算/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()  {
+    int t;
+    cin>>t;
+    int a[8]={0,1500,4500,9000,35000,55000,80000};
+    double f[8]={0.03,0.1,0.2,0.25,0.3,0.35,0.45};
+    int k;
+    for (k=0;k<7;k++)
+        if (t<a[k]) break;
+    k--;
+    for (int )
+    return 0;
+}
