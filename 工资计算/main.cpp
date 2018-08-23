e419b5c3668eff2c5b8794730e07d7ae4500aa83
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-23 20:43:25 +0800

    真难啊。。。

--- 工资计算/main.cpp
+++ 工资计算/main.cpp
@@ -1,15 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int sr[]={3500,3500+1500,3500+4500,3500+9000,3500+35000,3500+55000,3500+80000};
+int tr[]={3,10,20,25,30,35,45};
+const int SIZE=sizeof(sr)/sizeof(int);
+int range[SIZE];
+
 int main()  {
-    int t;
+    int t,s;
     cin>>t;
-    int a[8]={0,1500,4500,9000,35000,55000,80000};
-    double f[8]={0.03,0.1,0.2,0.25,0.3,0.35,0.45};
-    int k;
-    for (k=0;k<7;k++)
-        if (t<a[k]) break;
-    k--;
-    for (int )
+    range[0]=sr[0];
+    for (int i=1;i<SIZE;i++)    {
+        range[i]=range[i-1]+sr[i]-sr[i-1]
+            -(sr[i]-sr[i-1])*tr[i-1]/100;
+    }
+    int i;
+    for (i=0;i<SIZE;i++)
+        if (t<=range[i])    break;
+    s=sr[i-1]+(t-range[i-1])*100/(100-tr[i-1]);
+    cout<<s<<endl;
     return 0;
 }
