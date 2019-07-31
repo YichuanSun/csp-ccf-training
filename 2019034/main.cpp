4864c22c807ecb08c71975a39cabe3bef0dc23d1
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-07-31 20:17:02 +0800

    好像写不下去了。。。处理字符串能力还是不行啊

---
+++ 2019034/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define N 10005
+using namespace std;
+vector<string> vs[N];
+int main()  {
+    int n,t;
+    scanf("%d%d",&t,&n);
+    while (t--) {
+        for (int i=0;i<n;i++)
+    }
+    return 0;
+}
