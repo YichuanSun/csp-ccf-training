9de7995c14ca61c77f76059bab10ed52dace0ed1
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-09-14 20:01:12 +0800

    一年比一年菜了哟弟弟

--- 2018122/main.cpp
+++ 2018122/main.cpp
@@ -1,25 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int cal(int a);
-int r,y,g,n,k,t,tim=0;
-int main()  {
-    scanf("%d%d%d%d",&r,&y,&g,&n);
-    int cycle=r+y+g;
-    for (int i=0;i<n;i++)   {
-        scanf("%d%d",&k,&t);
-        int nowk;
-//        cout<<"This time \t"<<i<<' '<<nowk<<endl;
-//        cout<<"and nowk=\t"<<cal(nowk)<<endl;
-//        cout<<"and tim=\t"<<tim<<endl;
-        if (k==0)   tim+=t;
-        else if (k==1)  nowk=r;
-        else if (k==2)  nowk=r+g+t;
-        else nowk=r+t;
-        nowk%=cycle;
-        if (nowk>=0&&nowk<r)    tim+=r-nowk;
-        else if (nowk>=r&&nowk<r+g) ;
-        else tim+=r+g+y-nowk+r;
-    }
-    printf("%d\n",tim);
-    return 0;
+typedef long long ll;
+//先是红绿灯顺序搞错了，再是黄灯后需要接红灯，再是longlong型，服了
+int main()	{
+	ll r,y,g,n,k,t,tt=0;
+	scanf("%lld%lld%lld%lld",&r,&y,&g,&n);
+	ll cyc=r+y+g;
+	for (ll i=0;i<n;i++)	{
+		scanf("%lld%lld",&k,&t);
+		if (k==0)	tt+=t;
+		else if (k==1)	{
+			ll temp=tt+r-t;
+			if (temp%cyc<=r)
+				tt+=(r-temp%cyc);
+			else if (temp%cyc>=r+g)
+				tt+=(cyc-temp%cyc+r);
+		}
+		else if (k==2)	{
+			ll temp=tt+cyc-t;
+			if (temp%cyc<=r)
+				tt+=(r-temp%cyc);
+			else if (temp%cyc>=r+g)
+				tt+=(cyc-temp%cyc+r);
+		}
+		else if (k==3)	{
+			ll temp=tt+r+g-t;
+			if (temp%cyc<=r)
+				tt+=(r-temp%cyc);
+			else if (temp%cyc>=r+g)
+				tt+=(cyc-temp%cyc+r);
+		}
+		//cout<<"Now Round: "<<i+1<<'\t'<<tt<<endl;
+	}
+	printf("%lld\n",tt);
+	return 0;
 }
